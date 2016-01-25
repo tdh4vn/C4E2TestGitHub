485afdb0798bf5536a7c4d33318c7c40c3dd0e44
hiepsieuviet <hiepxanh@gmail.com>
2016-01-25 22:21:55 +0700

    hiep

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 tao vua code cai nay
 hiep vua code cai nay
 chim vua keu o dau
-chim cua ai vua keu ? :))
+chim cua ai vua keu? :))
 
 GitHub la he thong upload code va quan ly phien ban
 cap nhat code moi
